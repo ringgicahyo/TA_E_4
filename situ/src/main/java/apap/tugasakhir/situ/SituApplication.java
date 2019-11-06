0a2d8e0d79e3fb0242f60366a5bc59428a110bc3
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2019-11-06 21:30:32 +0700

    project: add situ project folder

---
+++ situ/src/main/java/apap/tugasakhir/situ/SituApplication.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.situ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SituApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SituApplication.class, args);
+    }
+
+}
---
+++ situ/src/test/java/apap/tugasakhir/situ/SituApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.situ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SituApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
