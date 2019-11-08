a274e62e7c3b45387044a6b60cacd8fa8090dd7c
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2019-11-08 16:55:27 +0700

    feat: add LowonganDb.java #2

---
+++ situ/src/main/java/apap/tugasakhir/situ/repository/LowonganDb.java
@@ -0,0 +1,12 @@
+package apap.tugasakhir.situ.repository;
+
+import apap.tugasakhir.situ.model.LowonganModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LowonganDb extends JpaRepository<LowonganModel, Integer> {
+    Optional<LowonganModel> findById(Integer id);
+}
