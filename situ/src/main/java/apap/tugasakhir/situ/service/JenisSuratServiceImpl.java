8da40750a36090222d3bebcccbff27ec4cccfd6f
alkautsarm <alkautsarm@gmail.com>
2019-11-08 09:55:07 +0700

    feat: add feature add-jenis-surat (still bad UI & relation to PengajuanSurat still commented) #1

---
+++ situ/src/main/java/apap/tugasakhir/situ/service/JenisSuratServiceImpl.java
@@ -0,0 +1,41 @@
+package apap.tugasakhir.situ.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import apap.tugasakhir.situ.model.JenisSuratModel;
+import apap.tugasakhir.situ.repository.JenisSuratDb;
+
+@Service
+public class JenisSuratServiceImpl implements JenisSuratService {
+
+  @Autowired
+  JenisSuratDb jenisSuratDb;
+
+  @Override
+  public boolean addJenisSurat(JenisSuratModel jenisSurat) {
+    
+    if(cekKesamaanNamaJenisSurat(jenisSurat.getNama())) {
+      return false;
+    }
+
+    jenisSuratDb.save(jenisSurat);
+    return true;
+  }
+
+  @Override
+  public boolean cekKesamaanNamaJenisSurat(String nama) {
+    List<JenisSuratModel> listJenisSurat = jenisSuratDb.findAll();
+
+    for(JenisSuratModel x : listJenisSurat) {
+      if(x.getNama().equals(nama)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+}
