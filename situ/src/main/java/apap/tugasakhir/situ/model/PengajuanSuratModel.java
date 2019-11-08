ef241f4b2a177e1f432f593b803e9e1fee8208a2
Mila Shania <shnmila@gmail.com>
2019-11-08 19:59:11 +0700

    refactor: PengajuanSuratModel change id from auto to identity

--- situ/src/main/java/apap/tugasakhir/situ/model/PengajuanSuratModel.java
+++ situ/src/main/java/apap/tugasakhir/situ/model/PengajuanSuratModel.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class PengajuanSuratModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotNull
