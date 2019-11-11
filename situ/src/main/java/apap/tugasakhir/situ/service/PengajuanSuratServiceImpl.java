044f277b4afb253733dffc0219971f15389e53de
Mila Shania <shnmila@gmail.com>
2019-11-11 22:43:50 +0700

    refactor: html daftar-pengajuan-surat'

--- situ/src/main/java/apap/tugasakhir/situ/service/PengajuanSuratServiceImpl.java
+++ situ/src/main/java/apap/tugasakhir/situ/service/PengajuanSuratServiceImpl.java
@@ -35,12 +35,12 @@ public class PengajuanSuratServiceImpl implements PengajuanSuratService{
     @Override
     public String deletePengajuanSurat(Integer id) {
         PengajuanSuratModel pengajuanSurat = pengajuanSuratDb.getById(id);
-        if (pengajuanSurat.getStatus().equals("Menunggu Persetujuan")){
+//        if (pengajuanSurat.getStatus().equals("Menunggu Persetujuan")){
             pengajuanSuratDb.delete(pengajuanSurat);
 //            System.out.println("Test masuk sini");
             return "Delete";
-        }
-        return "Fail Delete";
+//        }
+//        return "Fail Delete";
     }
 
     @Override
