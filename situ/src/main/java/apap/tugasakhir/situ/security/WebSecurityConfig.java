7284e9dd16224d25062b38af0e996f0008905595
Mila Shania <shnmila@gmail.com>
2019-11-11 18:28:32 +0700

    refactor: in memory username and pass

--- situ/src/main/java/apap/tugasakhir/situ/security/WebSecurityConfig.java
+++ situ/src/main/java/apap/tugasakhir/situ/security/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package apap.tugasakhir.situ.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,8 +31,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
     }
 
+    @Autowired
+    public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .passwordEncoder(encoder())
+                .withUser("nadiem").password(encoder().encode("makarim"))
+                .roles("Siswa");
+    }
+
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 }
+
