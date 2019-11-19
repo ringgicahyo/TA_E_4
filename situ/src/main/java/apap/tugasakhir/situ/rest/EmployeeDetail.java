f6195d97c42758b8452cbf2788364a01e3d4a431
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2019-11-19 19:16:57 +0700

    feat: add data transfer object for employee details from sivitas api #4

---
+++ situ/src/main/java/apap/tugasakhir/situ/rest/EmployeeDetail.java
@@ -0,0 +1,86 @@
+package apap.tugasakhir.situ.rest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EmployeeDetail {
+    @JsonProperty("idUser")
+    private String idUser;
+
+    @JsonProperty("nip")
+    private String nip;
+
+    @JsonProperty("nama")
+    private String nama;
+
+    @JsonProperty("tempatLahir")
+    private String tempatLahir;
+
+    @JsonProperty("tanggalLahir")
+    private Date tanggalLahir;
+
+    @JsonProperty("alamat")
+    private String alamat;
+
+    @JsonProperty("telepon")
+    private String telepon;
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+}
