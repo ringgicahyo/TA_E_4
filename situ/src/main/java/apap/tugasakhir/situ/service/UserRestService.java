f6e50917a695285db2951e171da82260c1083e80
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2019-11-19 19:20:37 +0700

    feat: add interface UserRestService #4

---
+++ situ/src/main/java/apap/tugasakhir/situ/service/UserRestService.java
@@ -0,0 +1,10 @@
+package apap.tugasakhir.situ.service;
+
+import apap.tugasakhir.situ.rest.EmployeeDetailResponse;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public interface UserRestService {
+    Mono<EmployeeDetailResponse> getUserProfile(String uuid);
+}
