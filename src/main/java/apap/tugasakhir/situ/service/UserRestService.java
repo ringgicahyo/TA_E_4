970a2edf91f481012e0ea0c841051e90006feae6
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2019-11-21 21:06:15 +0700

    feat: modify UserRestService interface to get api if user is teacher or student

--- src/main/java/apap/tugasakhir/situ/service/UserRestService.java
+++ src/main/java/apap/tugasakhir/situ/service/UserRestService.java
@@ -1,14 +1,15 @@
 package apap.tugasakhir.situ.service;
 
 import apap.tugasakhir.situ.rest.AddEmployeeResponse;
-import apap.tugasakhir.situ.rest.EmployeeDetailResponse;
+import apap.tugasakhir.situ.rest.LoggedUserDetailResponse;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public interface UserRestService {
-    Mono<EmployeeDetailResponse> getUserProfile(String uuid);
+    Mono<LoggedUserDetailResponse> getUserProfileForEmployee(String uuid);
+    Mono<LoggedUserDetailResponse> getUserProfileForTeacher(String uuid);
+    Mono<LoggedUserDetailResponse> getUserProfileForStudent(String uuid);
     Mono<AddEmployeeResponse> postAddEmployee(JSONObject data);
-
 }
