f7fd6379cb89765ef3510f7e2421c7aa1c40ea8a
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2019-11-21 16:26:35 +0700

    feat: add SiPerpusUserRoleDetail.java to get role data of user from siperpus #2

---
+++ situ/src/main/java/apap/tugasakhir/situ/rest/SiPerpusUserRoleDetail.java
@@ -0,0 +1,29 @@
+package apap.tugasakhir.situ.rest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SiPerpusUserRoleDetail {
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("nama")
+    private String nama;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
